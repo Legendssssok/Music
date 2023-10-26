effb57290f32f0dcf9c4eeeb9f0554285d6202cd
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-10-26 15:56:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,14 +20,14 @@ class Config(object):
     BOT_NAME = getenv("BOT_NAME", "\x40\x4d\x75\x73\x69\x63\x5f\x48\x65\x6c\x6c\x42\x6f\x74")   # dont put fancy texts here.
     BOT_PIC = getenv("BOT_PIC", "https://te.legra.ph/file/5d5642103804ae180e40b.jpg")           # put direct link to image here
     LEADERBOARD_TIME = getenv("LEADERBOARD_TIME", "3:00")   # time in 24hr format for leaderboard broadcast
-    LYRICS_API = getenv("LYRICS_API", None)             # from https://docs.genius.com/
+    LYRICS_API = "nOAsdqvMoEatfCtiPLx6UQZeFDhk08vTIraX2g8sXQFtwtG2l2GXEVDRM7hDX"           # from https://docs.genius.com/
     MAX_FAVORITES = int(getenv("MAX_FAVORITES", 30))    # max number of favorite tracks
     PLAY_LIMIT = int(getenv("PLAY_LIMIT", 0))           # time in minutes. 0 for no limit
     PRIVATE_MODE = getenv("PRIVATE_MODE", "off")        # "on" or "off" to enable/disable private mode
     SONG_LIMIT = int(getenv("SONG_LIMIT", 0))           # time in minutes. 0 for no limit
     TELEGRAM_IMG = getenv("TELEGRAM_IMG", None)         # put direct link to image here
     TG_AUDIO_SIZE_LIMIT = int(getenv("TG_AUDIO_SIZE_LIMIT", 104857600))     # size in bytes. 0 for no limit
-    TG_VIDEO_SIZE_LIMIT = int(getenv("TG_VIDEO_SIZE_LIMIT", 1073741824))    # size in bytes. 0 for no limit
+    TG_VIDEO_SIZE_LIMIT = int(getenv("TG_VIDEO_SIZE_LIMIT", 3073741824))    # size in bytes. 0 for no limit
     TZ = getenv("TZ", "Asia/Kolkata")   # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
     # do not edit these variables
